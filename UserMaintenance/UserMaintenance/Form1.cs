563c3c35d02d9a264119a7d325d606ff833745b3
Petra Horváth <petra25horvath@gmail.com>
2022-10-04 12:07:26 +0200

    adding user

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
@@ -18,6 +19,20 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.DisplayMember = "FullName";
+            listBox1.ValueMember = "ID";
+        }
+
+        BindingList<User> users = new BindingList<User>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User u = new User();
+            u.FirstName = textBox2.Text;
+            u.LastName = textBox1.Text;
+            users.Add(u);
         }
     }
 }
