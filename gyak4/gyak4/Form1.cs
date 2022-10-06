fd6e795c7d22b71bba9a4c70ecdbd0d77ac5964d
Petra Horváth <petra25horvath@gmail.com>
2022-10-06 17:59:02 +0200

    start Excel

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace gyak4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+
+        List<Flat> flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
+       
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            flats = context.Flats.ToList();
+        }
+        
+        
+
     }
 }
