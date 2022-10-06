4e4782814ac53090dc7a279e437ef77bcdfc5ecd
Petra Horváth <petra25horvath@gmail.com>
2022-10-06 18:08:30 +0200

    CreateExcel

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -22,18 +22,48 @@ namespace gyak4
         Excel.Workbook xlWb;
         Excel.Worksheet xlSheet;
 
-       
+             
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             flats = context.Flats.ToList();
         }
+        
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWb.ActiveSheet;
+
+                //CreateTable()
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg= string.Format("Error: {0}\nLine: {1}", ex.Message,ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close();
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
+        }
+
         
         
 
