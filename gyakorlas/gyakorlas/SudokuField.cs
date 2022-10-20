d97f09b8756fdef7f0e2aab70cfacb1364be7039
Petra Horváth <petra25horvath@gmail.com>
2022-10-20 16:38:05 +0200

    empty project

---
+++ gyakorlas/gyakorlas/SudokuField.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyakorlas
+{
+    class SudokuField : Button
+    {
+        public SudokuField()
+        {
+            Height = 30;
+            Width = Height;
+            BackColor = Color.White;
+            Value = 0;
+            MouseDown += SudokuField_MouseDown;
+        }
+
+        private void SudokuField_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) Value++;
+            if(e.Button == MouseButtons.Right) Value--;
+        }
+
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set { 
+                _value = value;
+                if (_value < 0) _value = 9;
+                if (_value > 9)  _value = 0;
+                if (_value == 0) Text = "";
+                else Text = _value.ToString();
+                
+            }
+        }
+
+    }
+}
