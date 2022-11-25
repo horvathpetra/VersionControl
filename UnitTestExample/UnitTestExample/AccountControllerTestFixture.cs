447e0dde76e465fc8d3c21c765f04917ed4745df
Petra Horváth <petra25horvath@gmail.com>
2022-11-25 18:53:39 +0100

    Regex

---
+++ UnitTestExample/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+        [
+            Test,
+             TestCase("abcd1234", false),
+             TestCase("irf@uni-corvinus", false),
+             TestCase("irf.uni-corvinus.hu", false),
+             TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+         TestCase("aaaaAAAA", false),
+        TestCase("ABCD1234", false),
+            TestCase("aaaaaaaa", false),
+            TestCase("aA123", false),
+            TestCase("Abcs1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualresult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualresult);
+        }
+    }
+}
