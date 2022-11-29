057752dfaaf3b9078f98b67664f078bf04c12ee1
Petra Horváth <petra25horvath@gmail.com>
2022-11-29 17:07:51 +0100

    loadData1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class Person
+    {
+        public  int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsAlive { get; set; }
+        public int NumberOfChildren { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+
+    
+}
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +17,32 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        List<Person> population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NumberOfChildren = int.Parse(line[2])
+                    });
+                }
+                
+            }
+            return population;
+
+
         }
     }
 }
